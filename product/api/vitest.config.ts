188b17bd4842ca5d1ed153b8e3939ca317dca706
Han <apricot13@users.noreply.github.com>
2025-07-24 14:54:28 +0100

    coverage

--- product/api/vitest.config.ts
+++ product/api/vitest.config.ts
@@ -9,6 +9,12 @@ export default defineConfig(({mode}) => ({
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
       reportOnFailure: true,
+      thresholds: {
+        lines: 60,
+        branches: 60,
+        functions: 60,
+        statements: 60,
+      },
     },
   },
 }));
