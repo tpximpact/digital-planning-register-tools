319bd70fe475d2d400efbbe2cc7e155c02c06b15
Han <apricot13@users.noreply.github.com>
2025-07-24 14:32:03 +0100

    Add ci/cd

--- product/api/vitest.config.ts
+++ product/api/vitest.config.ts
@@ -5,5 +5,10 @@ export default defineConfig(({mode}) => ({
   test: {
     // mode defines what ".env.{mode}" file to choose if exists
     env: loadEnv(mode, process.cwd(), ''),
+
+    coverage: {
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
   },
 }));
