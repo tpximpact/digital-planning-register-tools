60dbdea338e912c97cb14b4dfeebbe76e12cc1c1
Maddie Williams <maddie.williams1@gmail.com>
2025-07-25 12:31:56 +0100

    db init script and dbml converter script in package.json

--- product/api/src/scripts/init-db.ts
+++ product/api/src/scripts/init-db.ts
@@ -1,10 +1,12 @@
 import {db} from '../db';
+import {seedDatabase} from '../db/seed';
 import {setupDatabase} from '../db/setup-db';
 
 async function initialize() {
   console.log('Starting manual database initialization...');
   try {
     await setupDatabase();
+    await seedDatabase(db);
     console.log('Database initialization completed successfully.');
   } catch (error) {
     console.error('An error occurred during database initialization:', error);
