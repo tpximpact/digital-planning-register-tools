c32917be6e46dafbcd53f8e274ba23156ccfed27
Han <apricot13@users.noreply.github.com>
2025-07-29 14:17:48 +0100

    delete the lot

--- product/api/src/errors/planning-application-not-found.error.ts
+++
@@ -1,6 +0,0 @@
-export class PlanningApplicationNotFoundError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'PlanningApplicationNotFoundError';
-  }
-}
--- product/api/src/services/index.ts
+++
@@ -1,1 +0,0 @@
-export {default as PlanningApplicationService} from './PlanningApplication.service';
