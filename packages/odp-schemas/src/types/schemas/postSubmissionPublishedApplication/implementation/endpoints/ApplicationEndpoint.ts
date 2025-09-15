0b788b19ff54ceb742de75dd4dd3250c5980ea85
Han <apricot13@users.noreply.github.com>
2025-09-15 13:18:51 +0100

    ODP schemas

--- packages/odp-schemas/src/types/schemas/postSubmissionPublishedApplication/implementation/endpoints/ApplicationEndpoint.ts
+++ packages/odp-schemas/src/types/schemas/postSubmissionPublishedApplication/implementation/endpoints/ApplicationEndpoint.ts
@@ -1,4 +1,4 @@
-import { type Static } from '@sinclair/typebox'
+import { Type, type Static } from '@sinclair/typebox'
 import { ApiResponse } from '../../../postSubmissionApplication/implementation/ApiResponse'
 import { PostSubmissionPublishedApplication } from '../..'
 
@@ -12,4 +12,15 @@ export const PostSubmissionPublishedApplicationEndpoint = ApiResponse(
 )
 export type PostSubmissionPublishedApplicationEndpoint = Static<
   typeof PostSubmissionPublishedApplicationEndpoint
+>
+
+/**
+ * Parameters for the endpoint
+ */
+
+export const PostSubmissionPublishedApplicationParams = Type.Object({
+  id: Type.String()
+})
+export type PostSubmissionPublishedApplicationParams = Static<
+  typeof PostSubmissionPublishedApplicationParams
 >
