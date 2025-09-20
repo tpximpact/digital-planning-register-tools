f99daa0ae5fc3e0982e12ea4aed00437b9d73965
Han <apricot13@users.noreply.github.com>
2025-09-20 02:43:37 +0100

    removr api archive

--- apps/api/src/archive/middleware/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './authentication/index.middleware'
-export * from './getInfo/index.middleware'
-export * from './handleErrors/index.middleware'
--- apps/api/src/archive/services/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ApplicationService } from './Application.service'
