38e638d73412623fafa3c2075cacfe7cbb3ccca5
Han <apricot13@users.noreply.github.com>
2026-05-30 00:17:55 +0100

    add debug for memory usage

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -12,5 +12,12 @@ new Elysia().use(app()).listen(config.port, () => {
     console.log(
       `Rate Limit: ${config.rateLimitMax} requests per ${config.rateLimitDuration}ms`
     )
+
+    const used = process.memoryUsage()
+    console.log(
+      `Startup memory: ${Math.round(
+        used.heapUsed / 1024 / 1024
+      )}MB heap, ${Math.round(used.rss / 1024 / 1024)}MB rss`
+    )
   }
 })
